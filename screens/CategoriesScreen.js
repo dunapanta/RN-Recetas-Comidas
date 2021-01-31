4de512c782697acf054e336d48beb255ded5f9d5
Daniel Unapanta <dunapanta2@gmail.com>
2021-01-30 22:07:41 -0600

    Define screens structure of the proyect

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View,  Text, StyleSheet } from 'react-native'
+
+const CategoriesScreen = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+
+})
