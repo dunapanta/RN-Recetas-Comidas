42825e3fa440924931aec17b46657a2cb971704f
Daniel Unapanta <dunapanta2@gmail.com>
2021-02-01 19:53:13 -0600

    Grid Categories

---
+++ data/dummy-data.js
@@ -0,0 +1,14 @@
+import Category from '../models/category'
+
+export const CATEGORIES = [
+    new Category('c1', 'Italiana', '#f5428d'),
+    new Category('c2', 'Rápido y Sencillo', '#f54242'),
+    new Category('c3', 'Hamburguesas', '#f5a442'),
+    new Category('c4', 'Ecuatoriana', '#f5d142'),
+    new Category('c5', 'Ligero', '#368dff'),
+    new Category('c6', 'Exotico', '#41d95d'),
+    new Category('c7', 'Desayunos', '#9eecff'),
+    new Category('c8', 'Asiático', '#b9ffb0'),
+    new Category('c9', 'Francesa', '#ffc7ff'),
+    new Category('c10', 'Verano', '#47fced')
+]
