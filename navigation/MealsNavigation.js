fe9427bbc662cb3d3faeb86794fa9dbd1af2ddb6
Daniel Unapanta <dunapanta2@gmail.com>
2021-02-04 14:20:14 -0600

    Adding Icons to Tab Navigation

--- navigation/MealsNavigation.js
+++ navigation/MealsNavigation.js
@@ -1,6 +1,8 @@
+import React from 'react'
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
@@ -29,8 +31,32 @@ const MealsNavigator = createStackNavigator({
 })
 
 const MealsFavTabNavigator =  createBottomTabNavigator({
-    Meals: MealsNavigator,
-    Favorites: FavoritesScreen
+    Meals: { screen: MealsNavigator, navigationOptions: {
+        tabBarLabel: 'Categorías',
+        tabBarIcon: (tabInfo) => {
+            return (
+                <Ionicons 
+                    name='ios-restaurant' 
+                    size={25}
+                    color={tabInfo.tintColor} />
+                )
+        }
+    }},
+    Favorites: {screen: FavoritesScreen, navigationOptions:{
+        tabBarLabel: 'Favoritos',
+        tabBarIcon: (tabInfo) => {
+            return (
+                <Ionicons 
+                    name='ios-star' 
+                    size={25}
+                    color={tabInfo.tintColor} />
+                )
+        }
+    }}
+}, {
+    tabBarOptions: {
+        activeTintColor: Colors.secondaryColor
+    }
 })
 
 export default createAppContainer(MealsFavTabNavigator)
