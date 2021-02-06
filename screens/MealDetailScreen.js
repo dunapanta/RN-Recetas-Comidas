87ff4d13bc221c83131a88bce29358c22cc7569b
Daniel Unapanta <dunapanta2@gmail.com>
2021-02-06 14:12:00 -0600

    MeatDetailScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,23 +1,43 @@
 import React from 'react'
-import { View,  Text, StyleSheet, Button} from 'react-native'
+import { ScrollView, View, Image, Text, StyleSheet, Button} from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import { MEALS } from '../data/dummy-data'
 import HeaderButton from '../components/HeaderButton'
+import DefaultText from '../components/DefaultText'
+import DetailItem from '../components/DeatilItem'
 
-const MealDetailScreen = ({ navigation }) => {
+const MealDetailScreen = ({ navigation, duration, complexity, affordability }) => {
 
     const mealId = navigation.getParam('mealId')
 
     const selectedMeal = MEALS.find(meal => meal.id === mealId)
 
     return (
-        <View style={styles.screen}>
-            <Text>{selectedMeal.title}</Text>
-            <Button title="Volver Categorias" onPress={
-                () => {navigation.popToTop()
-            }} />
-        </View>
+        <ScrollView>
+            <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration}m</DefaultText>
+                <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Lista de Ingredientes</Text>
+            {selectedMeal.ingredients.map( ingredient => (
+                <DetailItem key={ingredient}>{ingredient}</DetailItem>
+            ))}
+
+            <Text style={styles.title}>Instrucciones paso a paso</Text>
+            {selectedMeal.steps.map( step => (
+                <DetailItem key={step}>{step}</DetailItem>
+            ))}
+
+            {/* <View style={styles.screen}>
+                <Text>{selectedMeal.title}</Text>
+                <Button title="Volver Categorias" onPress={
+                    () => {navigation.popToTop()
+                }} />
+            </View> */}
+        </ScrollView>
     )
 }
 
@@ -42,10 +62,19 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 }
 
 const styles = StyleSheet.create({
-screen:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+image:{
+    width: '100%',
+    height: 200
+},
+details:{
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around'
+},
+title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center'
 }
 })
 
